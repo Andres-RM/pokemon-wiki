ceb21dbe2ae6c138e56b05b9e4c0edba3ab2a2df
andres <ramireznadres1994@gmail.com>
2021-02-18 17:04:26 -0400

    feat(shared): create navbar component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import {AppRoutingModule} from '../app-routing.module';
+
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        AppRoutingModule
+    ],
+  declarations: [NavbarComponent],
+  exports: [NavbarComponent]
+})
+export class SharedModule { }
